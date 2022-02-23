5d40a2780e2ae323331e3752b6ac37124edac668
functioner <oierwhz@gmail.com>
2022-02-23 13:28:59 -0500

    add log diff in analyzer scripts

--- tool/bin/analyzer-zookeeper-2247.sh
+++ tool/bin/analyzer-zookeeper-2247.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 case_name=zookeeper-2247
 OUT_DIR=$HOME/tmp/bytecode/$case_name/sootOutput
 SRC_DIR=$SCRIPT_DIR/../../systems/$case_name
+diff_log="${SCRIPT_DIR}/../../ground_truth/$case_name/diff_log.txt"
 classes_dir="$SRC_DIR/build/classes"
 test_classes_dir="$SRC_DIR/build/test/classes"
 runtime_classes_dir=$SCRIPT_DIR/../runtime/target/classes
@@ -16,3 +17,5 @@ $SCRIPT_DIR/analyzer.sh \
 -o $HOME/tmp/bytecode/$case_name/sootOutput \
 -i $classes_dir $test_classes_dir $runtime_classes_dir \
 -fc $case_name \
+-fld $diff_log \
+
