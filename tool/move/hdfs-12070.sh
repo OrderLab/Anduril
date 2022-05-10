44ff2da25c52c9f5e68a1896b111dc4990cf5cdf
functioner <oierwhz@gmail.com>
2022-05-09 22:54:44 -0400

    update hdfs-12070 move script

---
+++ tool/move/hdfs-12070.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hdfs-12070
+src_dir=$SCRIPT_DIR/../../systems/$case_name
+target_dir="$HOME/tmp/bytecode/$case_name/classes"
+sootoutput_dir="$HOME/tmp/bytecode/$case_name/sootOutput"
+
+rm -rf $target_dir
+mkdir -p $target_dir
+
+defaultList="./org/apache/hadoop/hdfs/protocol/HdfsFileStatus.class
+./org/apache/hadoop/fs/PathHandle.class"
+
+for classes_dir in `find $src_dir/hadoop-common-project -name classes`; do
+for i in `cd $classes_dir && find . -name "*.class" && cd - >/dev/null 2>&1`; do
+  flag=""
+  for j in $defaultList; do
+    if [[ "$i" == "$j" ]]; then
+      flag="1"
+    fi
+  done
+  mkdir -p $(dirname $target_dir/$i)
+  if [[ -z "$flag" ]]; then
+    if [[ -f "$sootoutput_dir/$i" ]]; then
+      rsync -a $sootoutput_dir/$i $target_dir/$i
+    fi
+  else
+    if [[ -f "$classes_dir/$i" ]]; then
+      rsync -a $classes_dir/$i $target_dir/$i
+    fi
+  fi
+done
+done
+for classes_dir in `find $src_dir/hadoop-hdfs-project -name classes`; do
+for i in `cd $classes_dir && find . -name "*.class" && cd - >/dev/null 2>&1`; do
+  flag=""
+  for j in $defaultList; do
+    if [[ "$i" == "$j" ]]; then
+      flag="1"
+    fi
+  done
+  mkdir -p $(dirname $target_dir/$i)
+  if [[ -z "$flag" ]]; then
+    if [[ -f "$sootoutput_dir/$i" ]]; then
+      rsync -a $sootoutput_dir/$i $target_dir/$i
+    fi
+  else
+    if [[ -f "$classes_dir/$i" ]]; then
+      rsync -a $classes_dir/$i $target_dir/$i
+    fi
+  fi
+done
+done
+
+for test_classes_dir in `find $src_dir/hadoop-common-project -name test-classes`; do
+for i in `cd $test_classes_dir && find . -name "*.class" && cd - >/dev/null 2>&1`; do
+  flag=""
+  for j in $defaultList; do
+    if [[ "$i" == "$j" ]]; then
+      flag="1"
+    fi
+  done
+  mkdir -p $(dirname $target_dir/$i)
+  if [[ -z "$flag" ]]; then
+    if [[ -f "$sootoutput_dir/$i" ]]; then
+      rsync -a $sootoutput_dir/$i $target_dir/$i
+    fi
+  else
+    if [[ -f "$test_classes_dir/$i" ]]; then
+      rsync -a $test_classes_dir/$i $target_dir/$i
+    fi
+  fi
+done
+done
+for test_classes_dir in `find $src_dir/hadoop-hdfs-project -name test-classes`; do
+for i in `cd $test_classes_dir && find . -name "*.class" && cd - >/dev/null 2>&1`; do
+  flag=""
+  for j in $defaultList; do
+    if [[ "$i" == "$j" ]]; then
+      flag="1"
+    fi
+  done
+  mkdir -p $(dirname $target_dir/$i)
+  if [[ -z "$flag" ]]; then
+    if [[ -f "$sootoutput_dir/$i" ]]; then
+      rsync -a $sootoutput_dir/$i $target_dir/$i
+    fi
+  else
+    if [[ -f "$test_classes_dir/$i" ]]; then
+      rsync -a $test_classes_dir/$i $target_dir/$i
+    fi
+  fi
+done
+done
+
+rsync -ra $SCRIPT_DIR/../runtime/target/runtime-1.0-jar-with-dependencies.jar $target_dir
