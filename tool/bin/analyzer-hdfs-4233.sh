ef81933808a5151d516dcebd744025f27758df0a
functioner <oierwhz@gmail.com>
2022-05-17 22:46:50 -0400

    add hdfs-4233 analysis script

---
+++ tool/bin/analyzer-hdfs-4233.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hdfs-4233
+OUT_DIR=$HOME/tmp/bytecode/$case_name/sootOutput
+SRC_DIR=$SCRIPT_DIR/../../systems/$case_name
+diff_log="${SCRIPT_DIR}/../../ground_truth/$case_name/diff_log.txt"
+alldirs=$SCRIPT_DIR/../runtime/target/classes
+for i in `find $SRC_DIR/hadoop-hdfs-project -name "classes"`; do alldirs="$i $alldirs"; done
+for i in `find $SRC_DIR/hadoop-common-project -name "classes"`; do alldirs="$i $alldirs"; done
+#for i in `find $SRC_DIR/hadoop-hdfs-project -name "test-classes"`; do alldirs="$i $alldirs"; done
+#for i in `find $SRC_DIR/hadoop-common-project -name "test-classes"`; do alldirs="$i $alldirs"; done
+
+rm -rf ${OUT_DIR}
+mkdir -p ${OUT_DIR}
+
+JAVA_OPTS="-Danalysis.prefix=org.apache.hadoop -Danalysis.distributedMode=true" \
+$SCRIPT_DIR/analyzer.sh \
+-o $HOME/tmp/bytecode/$case_name/sootOutput \
+-i $alldirs \
+-fc $case_name \
+-m org.apache.hadoop.hdfs.server.namenode.NameNode \
+-sm org.apache.hadoop.hdfs.server.namenode.SecondaryNameNode org.apache.hadoop.hdfs.server.datanode.DataNode \
+-fld $diff_log \
+
