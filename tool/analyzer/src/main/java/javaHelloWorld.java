e600847aee86f8199a81dbec792108b042727e51
panjia <jpan26@jh.edu>
2022-05-28 02:58:52 -0400

    example java and scala main class

---
+++ tool/analyzer/src/main/java/javaHelloWorld.java
@@ -0,0 +1,6 @@
+
+public class javaHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ tool/analyzer/src/main/scala/scalaHello.scala
@@ -0,0 +1,5 @@
+class scalaHello {
+  def main(args: Array[String]): Unit = {
+    println("Hello scala")
+  }
+}
