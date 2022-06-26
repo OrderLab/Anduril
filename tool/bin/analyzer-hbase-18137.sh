33ddf1c298cf9e7968d9e12061ed0f148f31e468
functioner <oierwhz@gmail.com>
2022-06-25 23:25:35 -0400

    add hbase-18137 analyzer script

---
+++ tool/bin/analyzer-hbase-18137.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hbase-18137
+OUT_DIR=$HOME/tmp/bytecode/$case_name/sootOutput
+SRC_DIR=$SCRIPT_DIR/../../systems/$case_name
+diff_log="${SCRIPT_DIR}/../../ground_truth/$case_name/diff_log.txt"
+alldirs=$SCRIPT_DIR/../runtime/target/classes
+for i in `find $SRC_DIR -name "classes"`; do alldirs="$i $alldirs"; done
+# must exclude some, otherwise loading bytecode gets silent errors
+for i in `find $SRC_DIR -name "test-classes"`; do
+  if [[ "$i" != *"hbase-archetypes"* ]]; then
+    alldirs="$i $alldirs"
+  fi
+done
+
+extras=""
+for i in `head -n1 $SRC_DIR/target/cached_classpath.txt|tr ':' '\n'`; do
+  if [[ "$i" == *"hadoop-common"* ]]; then
+    extras="$i $extras"
+  fi
+  if [[ "$i" == *"google/guava"* ]]; then
+    extras="$i $extras"
+  fi
+  if [[ "$i" == *"protobuf-java"* ]]; then
+    extras="$i $extras"
+  fi
+done
+
+rm -rf ${OUT_DIR}
+mkdir -p ${OUT_DIR}
+
+JAVA_OPTS="-Danalysis.prefix=org.apache.hadoop.hbase" \
+$SCRIPT_DIR/analyzer.sh \
+-o $HOME/tmp/bytecode/$case_name/sootOutput \
+-i $alldirs \
+-fc $case_name \
+-fld $diff_log \
+
+#-x $extras \
