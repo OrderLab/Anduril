f49049edc19b6f1bda744af11803169a6df42619
functioner <oierwhz@gmail.com>
2022-06-26 22:27:40 -0400

    improve hbase-20492 analysis script

--- tool/move/hbase-20492.sh
+++ tool/move/hbase-20492.sh
@@ -10,6 +10,7 @@ sootoutput_dir="$HOME/tmp/bytecode/$case_name/sootOutput"
 rm -rf $target_dir
 
 cp -r $sootoutput_dir $target_dir
+rm -r $target_dir/runtime
 
 for i in `cat $SCRIPT_DIR/default-$case_name.txt`; do
   q=$(echo "$i"|sed 's#/src/main/java/#/target/classes/#')
