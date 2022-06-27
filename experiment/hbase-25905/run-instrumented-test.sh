4054c3430570fd61df65ecbd3798a42792c87173
functioner <oierwhz@gmail.com>
2022-06-27 01:59:36 -0400

    complete hbase-25905 experiment

---
+++ experiment/hbase-25905/run-instrumented-test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hbase-25905
+hb_dir="${SCRIPT_DIR}/../../systems/$case_name"
+classes_dir="$HOME/tmp/bytecode/${case_name}/classes"
+runtime_jar="$classes_dir/runtime-1.0-jar-with-dependencies.jar"
+for i in $SCRIPT_DIR/deps/*; do classes_dir="$classes_dir:$i";done
+jars="$SCRIPT_DIR"
+for i in `head -n1 $hb_dir/hbase-build-configuration/target/cached_classpath.txt|tr ':' '\n'`; do
+  if [[ "$i" == *"hbase"* ]]; then
+    if [[ "$i" == *"thirdparty"* ]]; then
+      jars="$i:$jars"
+    fi
+  else
+    jars="$i:$jars"
+  fi
+done
+for i in `find $JAVA_HOME -name "*.jar"`; do jars="$i:$jars"; done
+
+testcase="org.apache.hadoop.hbase.regionserver.wal.TestAsyncFSWALRollStuck"
+
+if [ -z "$(which node)" ]; then
+  if [ -f "$HOME/nodejs-install/bin/node" ]; then
+    NODEJS=$HOME/nodejs-install/bin/node
+  fi
+else
+  NODEJS=node
+fi
+GROUND_TRUTH=$SCRIPT_DIR/../../ground_truth/$case_name
+
+java -noverify \
+-Dlog4j.configurationFile=file:$SCRIPT_DIR/log4j2.xml \
+-cp $classes_dir:$jars:$runtime_jar \
+-DflakyAgent.logInject=false \
+-DflakyAgent.fixPointInjectionMode=true \
+-DflakyAgent.injectionId=1 \
+-DflakyAgent.injectionTimes=-1 \
+-DflakyAgent.fault=java.io.IOException \
+-DflakyAgent.traceFile=$SCRIPT_DIR/trace.txt \
+org.junit.runner.JUnitCore $testcase
