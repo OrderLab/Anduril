5f4f56eb5b54ce1176d3bfbc49a255d84dfc220b
functioner <oierwhz@gmail.com>
2022-06-26 22:46:47 -0400

    add zookeeper-4203 oracle

---
+++ experiment/zookeeper-4203/oracle.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const fs = require('fs')
+
+const graph_json = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
+const trials_dir = process.argv[3] + '/'
+
+const targets = new Set()
+
+graph_json.injections.forEach(injection => {
+  if (injection.hasOwnProperty('location')) {
+    const loc = injection['location']
+    if (loc['class'] == 'org.apache.zookeeper.server.quorum.Leader$LearnerCnxAcceptor$LearnerCnxAcceptorHandler' &&
+        injection['invocation'] == 'java.net.Socket accept()' &&
+        injection['exception'] == 'java.io.IOException') {
+      targets.add(injection.id)
+    }
+  }
+})
+
+console.log('target injections:', targets)
+
+function check(o) {
+  let symptom = false
+  fs.readFileSync(o).toString().split('\n').forEach(line => {
+    if (line.startsWith('java.lang.IllegalStateException: State error')) {
+      symptom = true
+    }
+  })
+  return symptom
+}
+
+let i = 0
+try {
+  for (i = 0; i < 1000000; i++) {
+    const json = JSON.parse(fs.readFileSync(trials_dir + 'injection-' + i + '.json'))
+    if (json.hasOwnProperty('id')) {
+      if (targets.has(json.id)) {
+        if (check(trials_dir + 'output-' + i + '.txt')) {
+          console.log(`exposure: ${i}`)
+        }
+      }
+    }
+  }
+} catch(e) {
+  console.log(`ends at ${i} with ${e}`)
+}
