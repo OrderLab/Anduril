974a9fa67c24133b9f6fc12e41f5acf30f716c00
functioner <oierwhz@gmail.com>
2022-06-26 22:44:05 -0400

    update zookeeper-2247 experiment

--- experiment/zookeeper-2247/iterative-experiment.sh
+++ experiment/zookeeper-2247/iterative-experiment.sh
@@ -19,7 +19,14 @@ testcase="org.apache.zookeeper.server.quorum.QuorumPeerMainTest"
 #byteman=""
 #for i in `find $HOME/.m2/repository/org/jboss/byteman/byteman/*/**.jar`; do byteman=$i; done
 
-NODEJS=node
+if [ -z "$(which node)" ]; then
+  if [ -f "$HOME/nodejs-install/bin/node" ]; then
+    NODEJS=$HOME/nodejs-install/bin/node
+  fi
+else
+  NODEJS=node
+fi
+
 GROUND_TRUTH=$SCRIPT_DIR/../../ground_truth/$case_name
 
 mkdir -p $SCRIPT_DIR/build
@@ -37,14 +44,18 @@ echo "Running experiment $id at $(date)"
 java \
 -cp $classes_dir:$jars:$SCRIPT_DIR \
 -Dbuild.test.dir=$SCRIPT_DIR/build \
+-DflakyAgent.trialTimeout=90 \
 -DflakyAgent.avoidBlockMode=true \
+-DflakyAgent.injectionOccurrenceLimit=3 \
 -DflakyAgent.feedback=true \
 -DflakyAgent.traceFile=$trials_dir/trace-$id.txt \
 runtime.TraceAgent $trials_dir $SCRIPT_DIR/tree.json $trials_dir/injection-$id.json org.junit.runner.JUnitCore $testcase \
 > $trials_dir/output-$id.txt
 sleep 1
-feedback="$($NODEJS $SCRIPT_DIR/diff-score.js $GROUND_TRUTH/good-run-log.txt $SCRIPT_DIR/trials/output-$id.txt $GROUND_TRUTH/diff_log.txt $SCRIPT_DIR/tree.json)"
+feedback="$($NODEJS $SCRIPT_DIR/diff-score.js $GROUND_TRUTH/good-run-log.txt $SCRIPT_DIR/trials/output-$id.txt $GROUND_TRUTH/diff_log.txt $SCRIPT_DIR/tree.json | paste -sd ' ' - )"
 sed -i "3 i \ \ \ \ \"feedback\"\:\ $feedback," $SCRIPT_DIR/trials/injection-$id.json
 id=$(($id + 1))
-sleep 1
+if [ $id -gt 1000000 ]; then
+  break
+fi
 done
