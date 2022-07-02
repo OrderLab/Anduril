7797909b2efb62c45e1f7c576bdcbf53c1ccf1fb
functioner <oierwhz@gmail.com>
2022-07-02 00:44:19 -0400

    add hbase-19608 move script

---
+++ tool/move/hbase-19608.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hbase-19608
+hb_dir=$SCRIPT_DIR/../../systems/$case_name
+target_dir="$HOME/tmp/bytecode/$case_name/classes"
+sootoutput_dir="$HOME/tmp/bytecode/$case_name/sootOutput"
+
+rm -rf $target_dir
+
+cp -r $sootoutput_dir $target_dir
+rm -r $target_dir/runtime
+
+for i in `cat $SCRIPT_DIR/default-$case_name.txt`; do
+  q=$(echo "$i"|sed 's#/src/main/java/#/target/classes/#')
+  p=$(echo "$i"|sed 's#^./.*/src/main/java/##')
+  rsync -a $hb_dir/$q $target_dir/$p
+done
+
+rsync -ra $SCRIPT_DIR/../runtime/target/runtime-1.0-jar-with-dependencies.jar $target_dir
