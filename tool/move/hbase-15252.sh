356de4abb12caf53d8392a65c913d96867cfbc10
tonyPan123 <jpan26@jh.edu>
2022-07-02 21:47:28 -0400

    add hbase-15252 move script

---
+++ tool/move/hbase-15252.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hbase-15252
+hb_dir=$SCRIPT_DIR/../../systems/$case_name
+target_dir="$HOME/tmp/bytecode/$case_name/classes"
+sootoutput_dir="$HOME/tmp/bytecode/$case_name/sootOutput"
+
+rm -rf $target_dir
+
+cp -r $sootoutput_dir $target_dir
+rm -r $target_dir/runtime
+
+rsync -ra $SCRIPT_DIR/../runtime/target/runtime-1.0-jar-with-dependencies.jar $target_dir
