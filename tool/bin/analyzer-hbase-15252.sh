15b3c511037d322156bba46f00502435c7303285
tonyPan123 <jpan26@jh.edu>
2022-07-02 21:11:56 -0400

    add hbase-15252 analyzer script

---
+++ tool/bin/analyzer-hbase-15252.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=hbase-15252
+OUT_DIR=$HOME/tmp/bytecode/$case_name/sootOutput
+SRC_DIR=$SCRIPT_DIR/../../systems/$case_name
+diff_log="${SCRIPT_DIR}/../../ground_truth/$case_name/diff_log.txt"
+alldirs=$SCRIPT_DIR/../runtime/target/classes
+for i in `find $SRC_DIR -name "classes"`; do alldirs="$i $alldirs"; done
+for i in `find $SRC_DIR -name "test-classes"`; do alldirs="$i $alldirs"; done
+
+extras=""
+for i in `head -n1 $SRC_DIR/target/cached_classpath.txt|tr ':' '\n'`; do
+  extras="$i $extras"
+done
+
+rm -rf ${OUT_DIR}
+mkdir -p ${OUT_DIR}
+
+JAVA_OPTS="-Danalysis.prefix=org.apache.hadoop.hbase" \
+$SCRIPT_DIR/analyzer.sh \
+-o $HOME/tmp/bytecode/$case_name/sootOutput \
+-i $alldirs \
+-fc $case_name \
+-fld $diff_log \
+-x $extras \
