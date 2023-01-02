e2d65d469e4eb4e5cf5b2ebcbdc50df3f1773f5a
functioner <oierwhz@gmail.com>
2023-01-01 22:58:59 -0500

    add zookeeper-3006 evaluation

---
+++ evaluation/zookeeper-3006/update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=${PWD##*/}                    # to assign to a variable
+case_name=${case_name:-/}               # to correct for the case where PWD=/
+
+case_name=$(printf '%s\n' "${PWD##*/}") # to print to stdout
+                                        # ...more robust than echo for unusual names
+                                        #    (consider a directory named -e or -n)
+rm -f $SCRIPT_DIR/good-run-log
+rm -f $SCRIPT_DIR/bad-run-log
+for i in $SCRIPT_DIR/../../ground_truth/$case_name/good-run-*; do cp -r $i $SCRIPT_DIR/good-run-log;break;done
+cp -r $SCRIPT_DIR/../../ground_truth/$case_name/bad-run-* $SCRIPT_DIR/bad-run-log
+cp $SCRIPT_DIR/../../tool/feedback/target/feedback-*-dependencies.jar $SCRIPT_DIR/feedback.jar
+cp $SCRIPT_DIR/../../tool/driver/target/driver-*-dependencies.jar $SCRIPT_DIR/driver.jar
