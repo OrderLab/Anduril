8cb455aceb83bb4da91ebc44f2b65f3cfffb0a2e
functioner <oierwhz@gmail.com>
2023-01-01 23:00:33 -0500

    add zookeeper-3157 evaluation

---
+++ evaluation/zookeeper-3157/run-instrumented-test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=zookeeper-3157
+zk_dir="${SCRIPT_DIR}/../../systems/$case_name"
+#btm_dir="${SCRIPT_DIR}/none"
+#target="$zk_dir"/zookeeper-server/target/
+classes_dir="$HOME/tmp/bytecode/$case_name/classes"
+#runtime_classes_dir="$HOME/tmp/bytecode/$case_name/runtime-classes"
+#testclasses_dir="$target/test-classes"
+
+jars="."
+for i in $zk_dir/build/lib/*.jar; do jars="$i:$jars"; done
+for i in $zk_dir/build/test/lib/*.jar; do jars="$i:$jars"; done
+for i in `find $HOME/.m2 -name "*.jar"|grep 'javax.json'|grep '1.1.4'`; do jars="$i:$jars"; done
+#for i in `find $HOME/.m2/repository/org/jboss/byteman/ -name "*.jar"`; do jars="$i:$jars"; done
+for i in `find $JAVA_HOME -name "*.jar"`; do jars="$i:$jars"; done
+testcase="org.apache.zookeeper.server.quorum.FuzzySnapshotRelatedTest"
+#byteman=""
+#for i in `find $HOME/.m2/repository/org/jboss/byteman/byteman/*/**.jar`; do byteman=$i; done
+
+mkdir -p $SCRIPT_DIR/build
+
+java \
+-cp $classes_dir:$jars \
+-Dbuild.test.dir=$SCRIPT_DIR/build \
+-DflakyAgent.logInject=true \
+-DflakyAgent.fixPointInjectionMode=true \
+-DflakyAgent.injectionId=1 \
+-DflakyAgent.injectionTimes=-1 \
+-DflakyAgent.fault=java.io.IOException \
+-DflakyAgent.traceFile=asdf \
+org.junit.runner.JUnitCore $testcase
