6bb35d45f504f73e87854d9184eb66241a4fed53
functioner <oierwhz@gmail.com>
2023-01-09 00:56:45 -0500

    amend hdfs-13039 evaluation scripts

--- evaluation/hdfs-13039/cluster/start-cluster.sh
+++ evaluation/hdfs-13039/cluster/start-cluster.sh
@@ -3,7 +3,6 @@
 workspace=$(cd "$(dirname "${BASH_SOURCE-$0}")"; pwd)
 
 HADOOP_CLASSPATH="$HADOOP_CLASSPATH" HADOOP_OPTS="$HADOOP_OPTS -DflakyAgent.pid=0" $workspace/start-namenode.sh
-#./client.sh ec -enablePolicy -policy XOR-2-1-1024k
 HADOOP_CLASSPATH="$HADOOP_CLASSPATH" HADOOP_OPTS="$HADOOP_OPTS -DflakyAgent.pid=1" $workspace/start-datanode.sh 1
 HADOOP_CLASSPATH="$HADOOP_CLASSPATH" HADOOP_OPTS="$HADOOP_OPTS -DflakyAgent.pid=2" $workspace/start-datanode.sh 2
 HADOOP_CLASSPATH="$HADOOP_CLASSPATH" HADOOP_OPTS="$HADOOP_OPTS -DflakyAgent.pid=3" $workspace/start-datanode.sh 3
---
+++ evaluation/hdfs-13039/run-instrumented-workload.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case_name=${PWD##*/}                    # to assign to a variable
+case_name=${case_name:-/}               # to correct for the case where PWD=/
+
+case_name=$(printf '%s\n' "${PWD##*/}") # to print to stdout
+                                        # ...more robust than echo for unusual names
+                                        #    (consider a directory named -e or -n)
+
+runtime_jar=$HOME/tmp/bytecode/$case_name/runtime-1.0-jar-with-dependencies.jar
+process_number=5
+trials_dir=$SCRIPT_DIR/tmp_trials
+
+rm -rf $trials_dir
+mkdir -p $trials_dir
+
+java \
+-Dlog4j.configuration=file:$SCRIPT_DIR/log4j.properties \
+-DflakyAgent.logInject=true \
+-DflakyAgent.fixPointInjectionMode=true \
+-DflakyAgent.injectionId=1 \
+-DflakyAgent.injectionTimes=-1 \
+-DflakyAgent.fault=java.io.IOException \
+-DflakyAgent.distributedMode=true \
+-DflakyAgent.disableAgent=true \
+-DflakyAgent.traceFile=$trials_dir/trace.txt \
+-jar $runtime_jar $process_number $trials_dir $SCRIPT_DIR/tree.json $SCRIPT_DIR/injection.json \
+> $SCRIPT_DIR/output.txt 2>&1 &
+pid=$!
+
+HADOOP_CLASSPATH="$runtime_jar" \
+INJECT_HADOOP_OPTS="-DflakyAgent.logInject=true -DflakyAgent.injectionPointsPath=$SCRIPT_DIR/tree.json" \
+$SCRIPT_DIR/cluster/reproduction.sh
+
+sleep 1
+java -jar $runtime_jar # shutdown
+sleep 1
+
+rm -rf $trials_dir
+
+rm -rf $SCRIPT_DIR/record-inject
+mkdir -p $SCRIPT_DIR/record-inject
+for ((i=0;i<process_number;i++)); do
+  mkdir -p $SCRIPT_DIR/record-inject/logs-$i
+  cp $SCRIPT_DIR/cluster/logs-$i/*.log $SCRIPT_DIR/record-inject/logs-$i/
+done
+
