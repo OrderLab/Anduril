6a0e4955ab9fd9685f32cc90a9d3119f7f82c120
panjia <jpan26@jh.edu>
2023-07-09 21:23:22 -0400

    symptom descripton of cassandra-6415

--- tool/feedback/src/main/scala/feedback/cases/Cassandra.scala
+++ tool/feedback/src/main/scala/feedback/cases/Cassandra.scala
@@ -7,4 +7,9 @@ final class Cassandra_17663 extends UnitTestWorkload {
   override val ok_is_good: Boolean = false
 }
 
+@unused
+final class Cassandra_6415 extends DistributedWorkload {
+  override val targetNode: Int = 0
+}
+
 
