2ef9288e246d27cff20c5eec605d367af68c7eca
Tony Pan <jpan26@jh.edu>
2024-09-12 14:44:49 -0400

    revise the argument into the reporter

--- evaluation/zookeeper-2247/check-zookeeper-2247.sh
+++ evaluation/zookeeper-2247/check-zookeeper-2247.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-java -jar reporter-1.0-SNAPSHOT-jar-with-dependencies.jar -t $1 -b -n zookeeerp-2247
+java -jar reporter-1.0-SNAPSHOT-jar-with-dependencies.jar -t $1 -s tree.json
